5e3f43a253e2cb8c7723ff80a0353a6fb11de1c8
danila-clown <daphetad@gmail.com>
2026-09-13 22:44:57 +0300

    [TASK-3] linter part 2

--- onischuk.danila/lab3/solution.ts
+++ onischuk.danila/lab3/solution.ts
@@ -1,9 +1,9 @@
 function difference<T>(arr1: T[], arr2: T[]): T[] {
   let results: T[] = [];
 
-  for (let item1 of arr1) {
+  for (const item1 of arr1) {
     let flag: boolean = true;
-    for (let item2 of arr2) {
+    for (const item2 of arr2) {
       if (item1 === item2) {
         flag = false;
         break;
